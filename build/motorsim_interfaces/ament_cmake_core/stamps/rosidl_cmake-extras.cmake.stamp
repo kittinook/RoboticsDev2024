3b1e2e5e5828407c773d1deaa2e77735cd647547
chawrewerwee <122732439+B-Paweekorn@users.noreply.github.com>
2024-08-21 10:57:17 +0700

    init branch

--- build/motorsim_interfaces/ament_cmake_core/stamps/rosidl_cmake-extras.cmake.stamp
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(motorsim_interfaces_IDL_FILES "srv/MotorSpawn.idl")
-set(motorsim_interfaces_INTERFACE_FILES "srv/MotorSpawn.srv;srv/MotorSpawn_Request.msg;srv/MotorSpawn_Response.msg")
--- build/motorsim_interfaces/motorsim_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tanakon/RoboticsDev2024/build/motorsim_interfaces/motorsim_interfaces__py
-# Build directory: /home/tanakon/RoboticsDev2024/build/motorsim_interfaces/motorsim_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
