77747c8ebda611cd896896d4d12bac673b2d210f
kittinook <kitti.thamrong@gmail.com>
2024-09-13 15:51:45 +0700

    Delete build directory

--- build/motorsim/CMakeFiles/ament_cmake_python_copy_motorsim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ament_cmake_python_copy_motorsim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ament_cmake_python_copy_motorsim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
