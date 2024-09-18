65f3e7fefa5cef5961cb743979b3b98cde479505
kittinook <kitti.thamrong@gmail.com>
2024-09-18 11:28:55 +0700

    Add files via upload

---
+++ launch/simple_display.launch.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+"""
+This program is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. 
+If not, see <https://www.gnu.org/licenses/>.
+
+created by Thanacha Choopojcharoen at CoXsys Robotics (2022)
+"""
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+import xacro    
+    
+def generate_launch_description():
+    
+    pkg = get_package_share_directory('example_description')
+    rviz_path = os.path.join(pkg,'config','display.rviz')
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        arguments=['-d', rviz_path],
+        output='screen')
+    
+    path_description = os.path.join(pkg,'robot','visual','my-robot.xacro')
+    robot_desc_xml = xacro.process_file(path_description).toxml()
+    #robot_desc_xml = xacro.process_file(path_description,mappings={'robot_name': namespace}).toxml()
+    
+    parameters = [{'robot_description':robot_desc_xml}]
+    #parameters.append({'frame_prefix':namespace+'/'})
+    robot_state_publisher = Node(package='robot_state_publisher',
+                                  executable='robot_state_publisher',
+                                  output='screen',
+                                  parameters=parameters
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
+
+    launch_description = LaunchDescription()
+    
+    launch_description.add_action(rviz)
+    launch_description.add_action(robot_state_publisher)
+    launch_description.add_action(joint_state_publisher_gui)
+    
+    return launch_description
